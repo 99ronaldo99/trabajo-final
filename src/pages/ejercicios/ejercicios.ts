6b3c610d27932b7d5c41900cca93b6e71e4c1043
ronaldo <99ronaldoo99@gmail.com>
2018-05-31 16:33:57 -0500

    pagina de ejercicios + colores terminado

--- src/pages/ejercicios/ejercicios.ts
+++ src/pages/ejercicios/ejercicios.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { BicepsPage } from '../biceps/biceps';
+import { TricepsPage } from '../triceps/triceps';
+import { HombrosPage } from '../hombros/hombros';
+import { PiernasPage } from '../piernas/piernas';
+import { AbdomenPage } from '../abdomen/abdomen';
+import { EspaldaPage } from '../espalda/espalda';
+import { GluteosPage } from '../gluteos/gluteos';
+import { PectoralesPage } from '../pectorales/pectorales';
+import { ModalController, } from 'ionic-angular';
 
-/**
- * Generated class for the EjerciciosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -14,12 +19,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ejercicios.html',
 })
 export class EjerciciosPage {
+   pet: string = "puppies";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   isAndroid: boolean = false;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public modalCtrl: ModalController) {
+    /*this.isAndroid = platform.is('android');*/
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EjerciciosPage');
   }
 
+  openTabs(){
+    this.navCtrl.setRoot(HomePage)
+  }
+
+   openBiceps(){
+    this.navCtrl.push(BicepsPage)
+  }
+
+  openTriceps(){
+    this.navCtrl.push(TricepsPage)
+  }
+
+  openPectorales(){
+    this.navCtrl.push(PectoralesPage)
+  }
+
+  openHombros(){
+    this.navCtrl.push(HombrosPage)
+  }
+
+  openAbdomen(){
+    this.navCtrl.push(AbdomenPage)
+  }
+
+  openPiernas(){
+    this.navCtrl.push(PiernasPage)
+  }
+
+  openGluteos(){
+    this.navCtrl.push(GluteosPage)
+  }
+
+  openEspalda(){
+    this.navCtrl.push(EspaldaPage)
+  }
+
 }
+
