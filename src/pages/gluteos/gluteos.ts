cb9f5b6f84c0de4fec01e810f9d09bb10e14c0b2
ronaldo <99ronaldoo99@gmail.com>
2018-06-06 11:34:01 -0500

    imagenes de los gluteos y pierna agregados

--- src/pages/gluteos/gluteos.ts
+++ src/pages/gluteos/gluteos.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ModalController,} from 'ionic-angular';
+import  {InformacionPage}from '../informacion/informacion'
 
 /**
  * Generated class for the GluteosPage page.
@@ -15,11 +17,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GluteosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+  	public navCtrl: NavController, 
+  	public navParams: NavParams,
+  	public modalCtrl: ModalController,) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GluteosPage');
+  }
+  openModal(characterNum) {
+
+    let modal = this.modalCtrl.create(InformacionPage, characterNum);
+    modal.present();
   }
 
 }
--- src/pages/piernas/piernas.ts
+++ src/pages/piernas/piernas.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ModalController,} from 'ionic-angular';
+import  {InformacionPage}from '../informacion/informacion'
 
 /**
  * Generated class for the PiernasPage page.
@@ -15,11 +17,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PiernasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+  	public navCtrl: NavController, 
+  	public navParams: NavParams,
+  		public modalCtrl: ModalController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PiernasPage');
   }
+
+  openModal(characterNum) {
+
+    let modal = this.modalCtrl.create(InformacionPage, characterNum);
+    modal.present();
+  }
+
 
 }
